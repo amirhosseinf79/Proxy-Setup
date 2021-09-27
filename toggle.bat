b138449c7ea7b021a922e086a1dbfa392e17e88e
Amirhossein <83694528+amirhosseinf79@users.noreply.github.com>
2021-09-28 02:14:48 +0330

    update

--- toggle.bat
+++ toggle.bat
@@ -1,3 +1,4 @@
+@echo off
 d:
-cd D:\Programing\Python\Projects\Proxy Setup
+cd D:\Programing\Python\Projects\Proxy-Setup
 python toggle.py
--- toggle.py
+++ toggle.py
@@ -1,16 +1,19 @@
 import os
 import time
 
-f = open("stats.txt")
 network = "Home"
+prim_dns = "178.22.122.100"
+sec_dns = "185.51.200.2"
+
+f = open("stats.txt")
 stat = f.readlines()[0]
 f.close()
 
 def toggleProxy():
 	f=open("stats.txt","w+")
 	if int(stat)==1:
-		os.system(f'netsh interface ip set dns name="{network}" static 178.22.122.100')
-		os.system(f'netsh interface ip add dns name="{network}" 185.51.200.2 index=2')
+		os.system(f'netsh interface ip set dns name="{network}" static {prim_dns}')
+		os.system(f'netsh interface ip add dns name="{network}" {sec_dns} index=2')
 		os.system("REG ADD \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyEnable /t REG_DWORD /d 0 /f")
 		f.write("0")
 		return "Proxy Disabled"
